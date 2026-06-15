f9c871bd73b99b7f6d9f1516f0d25bbf1a15eb8e
Tomas <tommycampos07@gmail.com>
2026-06-15 11:19:55 -0600

    Mas arreglos para simular

--- uvm/env/aligner_env.sv
+++ uvm/env/aligner_env.sv
@@ -4,7 +4,7 @@ class aligner_env extends uvm_env;
 
   aligner_virtualseq vseqr;
 
-  apb_agent apb_agnt;
+  apb_agent   apb_agnt;
   md_rx_agent md_rx_agnt;
   md_tx_agent md_tx_agnt;
 
@@ -14,16 +14,16 @@ class aligner_env extends uvm_env;
   aligner_coverage rx_cov;
   aligner_coverage tx_cov;
 
-  aligner_reg_block                 ral_model;
-  aligner_apb_adapter               apb_adapter;
-  uvm_reg_predictor #(apb_item)      apb_predictor;
+  aligner_reg_block            ral_model;
+  aligner_apb_adapter          apb_adapter;
+  uvm_reg_predictor #(apb_item) apb_predictor;
 
   function new(string name = "aligner_env",
 
                uvm_component parent = null);
 
     super.new(name, parent);
-
+    
   endfunction
 
   virtual function void build_phase(uvm_phase phase);
@@ -32,12 +32,12 @@ class aligner_env extends uvm_env;
 
     vseqr = aligner_virtualseq::type_id::create("vseqr", this);
 
-    apb_agnt = apb_agent::type_id::create("apb_agnt", this);
-    md_rx_agent = md_rx_agent::type_id::create("md_rx_agnt", this)
-    md_tx_agent = md_tx_agent::type_id::create("md_tx_agnt", this)
+    apb_agnt   = apb_agent::type_id::create("apb_agnt", this);
+    md_rx_agnt = md_rx_agent::type_id::create("md_rx_agnt", this);
+    md_tx_agnt = md_tx_agent::type_id::create("md_tx_agnt", this);
 
     ref_model = aligner_reference_model::type_id::create("ref_model", this);
-    scb       = aligner_scoreboard::type_id::create("scb", this)
+    scb       = aligner_scoreboard::type_id::create("scb", this);
 
     rx_cov = aligner_coverage::type_id::create("rx_cov", this);
     tx_cov = aligner_coverage::type_id::create("tx_cov", this);
@@ -57,7 +57,7 @@ class aligner_env extends uvm_env;
 
     super.connect_phase(phase);
 
-    vseqr.apb_seqr = apb_agnt.seqr;
+    vseqr.apb_seqr   = apb_agnt.seqr;
     vseqr.md_rx_seqr = md_rx_agnt.seqr;
     vseqr.md_tx_seqr = md_tx_agnt.seqr;
 
@@ -77,6 +77,7 @@ class aligner_env extends uvm_env;
     vseqr.ral_model = ral_model;
 
     `uvm_info("ENV", "aligner_env connect_phase completed", UVM_LOW)
+
   endfunction
 
 endclass
